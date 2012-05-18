808d1d19d7260049b85e3ac44dd559f590e1b1b8
pda-test <paul+githubtest@annesley.cc>
2012-05-18 08:03:11 -0700

    Updated webhack.js

--- webhack.js
+++ webhack.js
@@ -3,33 +3,38 @@ var width = canvas.width;
 var height = canvas.height;
 var c = canvas.getContext("2d");
 
-canvas.style.background = "lightgreen";
+canvas.style.background = "rgba(200, 240, 200, 1)";
 
 function draw(rotation) {
   c.fillStyle = "green";
   c.save();
-  c.translate(width / 2, height / 2);
+  c.translate(width / 2 + rotation * 200, height / 2);
   c.rotate(rotation);
+  for (var i = 0; i < 6; i++)
+    c.fillRect((i * 40) - 110, -120, 20, 60);
   c.fillRect(-120, -20, 100, 100);
   c.fillRect(20, -20, 100, 100);
-  c.clearRect(-80 + 20, 20 - 20, 20, 20);
-  c.clearRect(60 + 20, 20 - 20, 20, 20);
+  c.clearRect(-60, 0, 20, 20);
+  c.clearRect(80, 0, 20, 20);
   c.fillRect(-120, 120, 240, 40);
-  for (var i = 0; i < 6; i++) {
-    var offset = -120 + i * 40 + 10;
-    c.fillRect(offset, -120, 20, 60);
-  }
   c.restore();
 }
 
-i = 0;
-function animate() {
-  i++;
-  increment = Math.sin(i / 40);
-  c.fillStyle = "rgba(128, 255, 128, 0.05)";
+function clear() {
+  c.fillStyle = "rgba(200, 240, 200, 0.2)";
   c.fillRect(0, 0, width, height);
-  draw(increment);
+}
+
+var i = 0;
+function animate() {
+  clear();
+  draw(Math.sin(++i / 40));
   window.setTimeout(animate, 10);
 }
 
-animate();
+var started = false;
+window.addEventListener("click", function () {
+  if (!started) { started = true; animate(); }
+});
+
+draw(0);
