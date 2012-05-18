743a226ca538455fc00836278400f63271efad20
pda-test <paul+githubtest@annesley.cc>
2012-05-18 01:36:40 -0700

    Updated webhack.js

--- webhack.js
+++ webhack.js
@@ -5,15 +5,31 @@ var c = canvas.getContext("2d");
 
 canvas.style.background = "lightgreen";
 
-c.fillStyle = "green";
-c.translate((width - 240)/2, 100);
-c.fillRect(0, 100, 100, 100);
-c.fillRect(140, 100, 100, 100);
-c.clearRect(40 + 20, 140 - 20, 20, 20);
-c.clearRect(180 + 20, 140 - 20, 20, 20);
-c.fillRect(0, 240, 240, 40);
-
-for (var i = 0; i < 6; i++) {
-  var offset = i * 40 + 10;
-  c.fillRect(offset, 0, 20, 60);
+function draw(rotation) {
+  c.fillStyle = "green";
+  c.save();
+  c.translate(width / 2, height / 2);
+  c.rotate(rotation);
+  c.fillRect(-120, -20, 100, 100);
+  c.fillRect(20, -20, 100, 100);
+  c.clearRect(-80 + 20, 20 - 20, 20, 20);
+  c.clearRect(60 + 20, 20 - 20, 20, 20);
+  c.fillRect(-120, 120, 240, 40);
+  for (var i = 0; i < 6; i++) {
+    var offset = -120 + i * 40 + 10;
+    c.fillRect(offset, -120, 20, 60);
+  }
+  c.restore();
 }
+
+i = 0;
+function animate() {
+  i++;
+  increment = Math.sin(i / 40);
+  c.fillStyle = "rgba(128, 255, 128, 0.05)";
+  c.fillRect(0, 0, width, height);
+  draw(increment);
+  window.setTimeout(animate, 10);
+}
+
+animate();
